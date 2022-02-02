5ed4bab1327ba5181de4a7f7c805d41fdc28f85b
guplee <aleksandr.engoyan@gmail.com>
2022-02-02 16:00:12 +0300

    add test for exception that doest work

--- src/test/java/com/sber/jnp/app/tests/JSONReadTests.java
+++ src/test/java/com/sber/jnp/app/tests/JSONReadTests.java
@@ -1,13 +1,20 @@
-package com.sber.jnp.tests;
+package com.sber.jnp.app.tests;
 
 import com.sber.jnp.app.*;
+import com.sber.jnp.app.exceptions.InvalidArgumentsException;
+
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.function.Executable;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class JSONReadTests {
-	@Test
-	public void	ProgramArguments() {
-
+	@Test()
+	public void	WrongProgramArguments() {
+		Throwable exception = assertThrows(
+				InvalidArgumentsException.class, () -> {
+					Main.main(new String[]{"--fileFile Test"});
+				});
 	}
 
 }
