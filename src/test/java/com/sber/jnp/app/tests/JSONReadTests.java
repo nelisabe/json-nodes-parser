6c1212a1f2ef54ecc5e309ae503c42bb646cc4c3
guplee <aleksandr.engoyan@gmail.com>
2022-02-03 16:06:46 +0300

    add tests for wrong file exceptions

--- src/test/java/com/sber/jnp/app/tests/JSONReadTests.java
+++ src/test/java/com/sber/jnp/app/tests/JSONReadTests.java
@@ -1,15 +1,16 @@
 package com.sber.jnp.app.tests;
 
 import com.sber.jnp.app.*;
+import com.sber.jnp.app.exceptions.IOErrorReadingJsonException;
 import com.sber.jnp.app.exceptions.InvalidArgumentsException;
 
+import com.sber.jnp.app.exceptions.WrongFileException;
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.function.Executable;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class JSONReadTests {
-	@Test()
+	@Test
 	public void	WrongProgramArguments() {
 		assertThrows(
 				InvalidArgumentsException.class, () -> {
@@ -31,5 +32,17 @@ public class JSONReadTests {
 				InvalidArgumentsException.class, () -> {
 					Main.main(new String[]{"--File File File"});
 				});
+	}
+	@Test
+	public void WrongFile() {
+		assertThrows(IOErrorReadingJsonException.class, () -> {
+			Main.main(new String[]{"--jsonFile NoSuchFile.json"});
+		});
+		assertThrows(WrongFileException.class, () -> {
+			Main.main(new String[]{"--jsonFile Wrong.json"});
+		});
+		assertThrows(WrongFileException.class, () -> {
+			Main.main(new String[]{"--jsonFile json.wr"});
+		});
 	}
 }
