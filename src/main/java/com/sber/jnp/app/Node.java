a6854133ef1ae59123a38ad7747046b01bcfb114
guplee <aleksandr.engoyan@gmail.com>
2022-02-03 16:08:33 +0300

    changes simple array to ArrayList

--- src/main/java/com/sber/jnp/app/Node.java
+++ src/main/java/com/sber/jnp/app/Node.java
@@ -1,8 +1,10 @@
 package com.sber.jnp.app;
 
+import java.util.ArrayList;
+
 public class Node {
 	String  name;
 	Color   color; // red, green, blue
 	int     value; // 0 - 100
-	Node[]  children;
+	ArrayList<Node> children;
 }
