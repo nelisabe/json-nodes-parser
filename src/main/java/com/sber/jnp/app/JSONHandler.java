bf2b4981613ad2ba30334e8606ecc50bb444150e
guplee <aleksandr.engoyan@gmail.com>
2022-02-03 16:08:08 +0300

    add file extention check

--- src/main/java/com/sber/jnp/app/JSONHandler.java
+++ src/main/java/com/sber/jnp/app/JSONHandler.java
@@ -2,7 +2,7 @@ package com.sber.jnp.app;
 
 import com.google.gson.Gson;
 import com.sber.jnp.app.exceptions.IOErrorReadingJsonException;
-import com.sber.jnp.app.exceptions.InvalidArgumentsException;
+import com.sber.jnp.app.exceptions.WrongFileException;
 
 import java.io.*;
 import java.nio.file.Files;
@@ -18,22 +18,35 @@ public class JSONHandler {
 		String  jsonString;
 
 		jsonString = readJsonFile(jsonFilePath);
-		node = gson.fromJson(jsonString, Node.class);
-		System.out.println(node.name);
-		System.out.println(node.value);
-		System.out.println(node.color);
+		try {
+			node = gson.fromJson(jsonString, Node.class);
+		} catch (Exception exception) {
+			throw new WrongFileException(exception);
+		}
 	}
 
 	private static String	readJsonFile(String jsonFilePath) {
 		String	json;
 
+		checkFileExtension(jsonFilePath);
 		try {
 			json = new String(Files.readAllBytes(Paths.get(jsonFilePath)));
 		} catch (IOException exception) {
-			throw new IOErrorReadingJsonException();
+			throw new IOErrorReadingJsonException(exception);
 		}
 		return json;
 	}
+
+	private static void		checkFileExtension(String jsonFilePath) {
+		int		indexOfLastDot;
+		String 	message = "invalid file extension";
+
+		if ((indexOfLastDot = jsonFilePath.lastIndexOf('.')) == -1)
+			throw new WrongFileException(message);
+		if (!jsonFilePath.substring(indexOfLastDot + 1).equals("json"))
+			throw new WrongFileException(message);
+	}
+
 	public static void  save(String jsonFilePath) {
 
 	}
