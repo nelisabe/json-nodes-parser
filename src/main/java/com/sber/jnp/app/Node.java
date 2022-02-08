625b062a22dbb387b1a495580f392a98a3aafc98
nelisabe <aleksandr.engoyan@gmail.com>
2022-02-08 17:37:48 +0300

    add getters and setters

--- src/main/java/com/sber/jnp/app/Node.java
+++ src/main/java/com/sber/jnp/app/Node.java
@@ -3,8 +3,29 @@ package com.sber.jnp.app;
 import java.util.ArrayList;
 
 public class Node {
-	String  name;
-	Color   color; // red, green, blue
-	int     value; // 0 - 100
-	ArrayList<Node> children;
+	private String  		name;
+	private Color   		color; // red, green, blue
+	private int     		value; // 0 - 100
+	private ArrayList<Node> children;
+
+	public String getName() {
+		return name;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public ArrayList<Node> getChildren() {
+		return children;
+	}
+
+	@Override
+	public String	toString() {
+		return "name: " + name + ", value: " + value + ", color: " + color.toString();
+	}
 }
