ec6171e1a11d36a7e1e7aafba738c17fd64a65b8
nelisabe <aleksandr.engoyan@gmail.com>
2022-02-09 18:12:14 +0300

    add tests for error when get iterator or node before read

--- src/test/java/com/sber/jnp/app/tests/JSONHandlerReadSaveTests.java
+++ src/test/java/com/sber/jnp/app/tests/JSONHandlerReadSaveTests.java
@@ -86,4 +86,64 @@ public class JSONHandlerReadSaveTests {
 		Files.deleteIfExists(Paths.get(resultJson));
 		Files.deleteIfExists(Paths.get(jsonFile));
 	}
+
+	@Test
+	public void	GetIteratorByPath() {
+		JSONHandler		jsonHandler = new JSONHandler();
+		Node			node;
+
+		jsonHandler.read("ComplexTree.json");
+		node = jsonHandler.getNode("A/F/O/");
+		assertEquals("O", node.getName());
+		assertEquals(44, node.getValue());
+		node = jsonHandler.getNode("A/W/F/C/D/W/B/");
+		assertEquals("B", node.getName());
+		assertEquals(2, node.getValue());
+		node = jsonHandler.getNode("A/M/O/");
+		assertEquals("O", node.getName());
+		assertEquals(25, node.getValue());
+		node = jsonHandler.getNode("A/");
+		assertEquals("A", node.getName());
+		assertEquals(10, node.getValue());
+	}
+
+	@Test
+	public void	WrongIteratorPath() {
+		JSONHandler		jsonHandler = new JSONHandler();
+
+		jsonHandler.read("ComplexTree.json");
+		assertThrows(InvalidInternalJsonPathException.class, () -> {
+			jsonHandler.getNode("");
+		});
+		assertThrows(InvalidInternalJsonPathException.class, () -> {
+			jsonHandler.getNode("A/D/P");
+		});
+		assertThrows(InvalidInternalJsonPathException.class, () -> {
+			jsonHandler.getNode("O/A/A");
+		});
+		assertThrows(InvalidInternalJsonPathException.class, () -> {
+			jsonHandler.getNode("/A/D/P");
+		});
+		assertThrows(InvalidInternalJsonPathException.class, () -> {
+			jsonHandler.getNode("/A/W/F/C/D/O");
+		});
+	}
+
+	@Test
+	public void	GetIteratorBeforeRead() {
+		assertThrows(NoJsonObjectReadException.class, () -> {
+			JSONHandler jsonHandler = new JSONHandler();
+			jsonHandler.iterator();
+			jsonHandler.iterator((x, y) ->
+					x.getValue() < y.getValue() ? x : y);
+		});
+	}
+
+	@Test
+	public void	GetNodeBeforeRead() {
+		assertThrows(NoJsonObjectReadException.class, () -> {
+			JSONHandler jsonHandler = new JSONHandler();
+			jsonHandler.getNode("A/");
+		});
+	}
 }
