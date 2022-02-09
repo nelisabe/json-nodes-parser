6027d4a26ec1c0135dc75d1334e1b075d77b31af
nelisabe <aleksandr.engoyan@gmail.com>
2022-02-09 18:10:03 +0300

    moved getNode tests from this class

--- src/test/java/com/sber/jnp/app/tests/JSONIteratorTests.java
+++ src/test/java/com/sber/jnp/app/tests/JSONIteratorTests.java
@@ -214,46 +214,4 @@ public class JSONIteratorTests {
 		});
 		Files.deleteIfExists(Paths.get(jsonFile));
 	}
-
-	@Test
-	public void	GetIteratorByPath() {
-		JSONHandler		jsonHandler = new JSONHandler();
-		Node			node;
-
-		jsonHandler.read("ComplexTree.json");
-		node = jsonHandler.getNode("A/F/O/");
-		assertEquals("O", node.getName());
-		assertEquals(44, node.getValue());
-		node = jsonHandler.getNode("A/W/F/C/D/W/B/");
-		assertEquals("B", node.getName());
-		assertEquals(2, node.getValue());
-		node = jsonHandler.getNode("A/M/O/");
-		assertEquals("O", node.getName());
-		assertEquals(25, node.getValue());
-		node = jsonHandler.getNode("A/");
-		assertEquals("A", node.getName());
-		assertEquals(10, node.getValue());
-	}
-
-	@Test
-	public void	WrongIteratorPath() {
-		JSONHandler		jsonHandler = new JSONHandler();
-
-		jsonHandler.read("ComplexTree.json");
-		assertThrows(InvalidInternalJsonPathException.class, () -> {
-			jsonHandler.getNode("");
-		});
-		assertThrows(InvalidInternalJsonPathException.class, () -> {
-			jsonHandler.getNode("A/D/P");
-		});
-		assertThrows(InvalidInternalJsonPathException.class, () -> {
-			jsonHandler.getNode("O/A/A");
-		});
-		assertThrows(InvalidInternalJsonPathException.class, () -> {
-			jsonHandler.getNode("/A/D/P");
-		});
-		assertThrows(InvalidInternalJsonPathException.class, () -> {
-			jsonHandler.getNode("/A/W/F/C/D/O");
-		});
-	}
 }
