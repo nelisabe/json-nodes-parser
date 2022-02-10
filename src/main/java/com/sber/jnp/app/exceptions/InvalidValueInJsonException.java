0a1c0e976afffa285ead005a48ea8b79dd6ae6fe
nelisabe <aleksandr.engoyan@gmail.com>
2022-02-10 14:41:25 +0300

    exception for errors in json file values

---
+++ src/main/java/com/sber/jnp/app/exceptions/InvalidValueInJsonException.java
@@ -0,0 +1,7 @@
+package com.sber.jnp.app.exceptions;
+
+public class InvalidValueInJsonException extends RuntimeException {
+	public InvalidValueInJsonException(String message) {
+		super(message);
+	}
+}
